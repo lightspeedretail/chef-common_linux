d30b4a14abfbc23d919999d59dbfdcd62a30c5f5
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-01-04 13:37:38 -0500

    Provide non-ec2 apt urls

--- attributes/ubuntu.rb
+++ attributes/ubuntu.rb
@@ -2,7 +2,12 @@
 default[:apt][:compile_time_update] = true
 
 default[:ubuntu].tap do |config|
-  config[:archive_url] = "http://us-east-1.ec2.archive.ubuntu.com.s3.amazonaws.com"
+  config[:archive_url] = 
+    if node[:ec2]
+      "http://us-east-1.ec2.archive.ubuntu.com.s3.amazonaws.com"
+    else
+      "http://archive.ubuntu.com/ubuntu"
+    end
   config[:security_url] = "http://security.ubuntu.com/ubuntu"
   config[:components] = "main restricted universe multiverse"
 end
