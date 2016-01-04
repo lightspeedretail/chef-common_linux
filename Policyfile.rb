943a36b370f2a545505aa478ce165e8a8f8039d7
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-01-04 10:13:28 -0500

    Update PolicyFile

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,17 +1,14 @@
-name "chef_common_linux"
+name "common_linux"
 
 default_source :supermarket
 #default_source :chef_server
 
 run_list(
-  "chef_common_linux::default"
+  "common_linux::default"
 )
 
 # Attributes
 #
-default[:authorization][:sudo][:groups] = %w(devops)
-default[:openssh][:server][:allow_groups] = %w(devops deploy)
-
 default[:ntp][:servers] = %w(
   0.amazon.pool.ntp.org
   1.amazon.pool.ntp.org
@@ -31,26 +28,7 @@ default[:common][:packages].tap do |config|
   config["ruby1.9.1-dev"] = true
 end
 
-default[:common][:linux].tap do |config|
-  config[:sudoers][:ubuntu].tap do |sudoer|
-    sudoer[:user] = "ubuntu"
-    sudoer[:runas] = "root"
-    sudoer[:nopasswd] = true
-  end
-
-  config[:sudoers][:deploy].tap do |sudoer|
-    sudoer[:group] = "deploy"
-    sudoer[:runas] = "root"
-    sudoer[:nopasswd] = true
-    sudoer[:commands] = ["/usr/bin/chef-client"]
-  end
-
-  config[:sudoers]["90-cloud-init-users"].tao do |sudoer|
-    sudoer[:action] = :remove
-  end
-end
-
 # Cookbooks
 #
-cookbook "chef_common_linux", path: "."
+cookbook "common_linux", path: "."
 cookbook "rsyslog_ng", path: "../rsyslog_ng"
