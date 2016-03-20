65a56d332de68b86bfa4957228bde9c46f2f2d99
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-20 19:35:54 -0400

    Update common_timezone to support whyrun

--- resources/timezone.rb
+++ resources/timezone.rb
@@ -1,20 +1,30 @@
 
-resource_name :timezone
+resource_name :common_timezone
 
 property :timezone,
   kind_of: String,
   default: lazy { |r| r.node[:common_linux][:timezone] || "Etc/UTC" },
   required: true
 
+# Ensure that the resource is applied regardless of whether we are in why_run
+# or standard mode.
+#
+# Refer to chef/chef#4537 for this uncommon syntax
+action_class do
+  def whyrun_supported?
+    true
+  end
+end
+
 action :set do
   template "/etc/timezone" do
     variables timezone: timezone
-    notifies :run, "bash[reload timezone]"
+    notifies :run, "execute[reload timezone]"
     notifies :restart, "service[cron]"
   end
 
-  bash "reload timezone" do
-    code "/usr/sbin/dpkg-reconfigure -f noninteractive tzdata"
+  execute "reload timezone" do
+    command "/usr/sbin/dpkg-reconfigure -f noninteractive tzdata"
     action :nothing
   end
 
